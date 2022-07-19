9d3cead03a1a0cd8379f45095ed47d3c64e86d50
Mr.Kangs <gykang00@gmail.com>
2022-07-19 01:47:40 -0700

    updates
    Co-authored-by: Marky Jordan <markyjordan@users.noreply.github.com>

--- public/Form.js
+++ public/Form.js
@@ -1,16 +1,24 @@
-function uploadRecord(html_file){
+function uploadRecord(){
 
-    var forms = document.getElementById("forms");
-
-
-    if(confirm("Are you sure you want to upload this record?")){
-        console.log("Uploaded");
-        forms.action = html_file;
-        // We can put validation if there is any missing fields after they confirm it or not.
-    } else{
-        console.log("Cancelled");
-        console.log(len(forms));
-        // This will simply hide the pop up and remain in the page with the values
-    }
+    console.log("Uploaded");
 
 }
+
+
+// var forms = document.getElementById("forms");
+//     cache_ID = [];
+//     cache_value = [];
+//     for (var i = 0; i < forms.length; i++) {
+//         cache_ID.push(forms[i].id);
+//         cache_value.push(forms[i].value);
+//     }
+
+//     if(confirm("Are you sure you want to upload this record?")){
+//         console.log("Uploaded");
+//         console.log(forms);
+//         forms.action = html_file;
+//         // We can put validation if there is any missing fields after they confirm it or not.
+//     } else{
+//         console.log("Cancelled");
+//         // This will simply hide the pop up and remain in the page with the values
+//     }
