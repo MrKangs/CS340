260e6494fda3f647bddc53978aabc7d8961dd931
Mr.Kangs <gykang00@gmail.com>
2022-07-22 02:45:46 -0700

    able to connect to the school database

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import os
+from flask_mysqldb import MySQL
+from flask import request
+import database.autogenerator as pk_generator
 
 # Configuration
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
+app.config['MYSQL_USER'] = 'cs340_kangken'
+app.config['MYSQL_PASSWORD'] = '8280' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_kangken'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+
+mysql = MySQL(app)
+
+# Variables
+pk_userAccounts = 0
+pk_fiatWallets = 0
+pk_dogecoinWallets = 0
+pk_exchangeOrders = 0
+pk_dogecoinTransactions = 0
+externalWalletID = set()
+transactionHash = set()
 
 # Routes
 
@@ -11,10 +31,80 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-# Listener
+@app.route('/userAccountsEntity.html', methods=["POST", "GET"])
+def user():
+    if request.method == "GET":
+        query = "SELECT * FROM userAccounts"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+    return render_template("userAccountsEntity.html", users=data)
 
+@app.route('/userAccountsForm.html')
+def userform():
+    return render_template("userAccountsForm.html")
+
+@app.route('/fiatWalletsEntity.html', methods=["POST", "GET"])
+def fiatwallet():
+    if request.method == "GET":
+        query = "SELECT * FROM fiatWallets"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+    return render_template("fiatWalletsEntity.html", fiatwallets=data)
+
+@app.route('/fiatWalletsForm.html')
+def fiatwalletform():
+    return render_template("fiatWalletsForm.html")
+
+@app.route('/dogecoinWalletsEntity.html', methods=["POST", "GET"])
+def dogecoinwallet():
+    if request.method == "GET":
+        query = "SELECT * FROM dogecoinWallets"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+    return render_template("dogecoinWalletsEntity.html", dogecoinWallets=data)
+
+@app.route('/dogecoinWalletsForm.html')
+def dogecoinwalletform():
+    return render_template("dogecoinWalletsForm.html")
+
+@app.route('/exchangeOrdersEntity.html', methods=["POST", "GET"])
+def exchangeorder():
+    if request.method == "GET":
+        query = "SELECT * FROM exchangeOrders"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+    return render_template("exchangeOrdersEntity.html", exchangeOrders=data)
+
+@app.route('/exchangeOrdersForm.html')
+def exchangeorderform():
+    return render_template("exchangeOrdersForm.html")
+
+@app.route('/dogecoinTransactionsEntity.html', methods=["POST", "GET"])
+def dogecointransaction():
+    if request.method == "GET":
+        query = "SELECT * FROM dogecoinTransactions"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+    return render_template("dogecoinTransactionsEntity.html",transactions=data)
+
+@app.route('/dogecoinTransactionsForm.html')
+def dogecointransactionform():
+    return render_template("dogecoinTransactionsForm.html")
+
+@app.route('/index.html')
+def reroute():
+    return redirect(url_for("root"))
+
+# Listener
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 36439))
 
 
     app.run(port=port, debug=True)
+
+
---
+++ flask/database/autogenerator.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+def generate_pk_userAccounts(number:int):
+    pk_value = str("U" + number)
+    number += 1
+    return pk_value, number
+
+def generate_pk_fiatWallets(number:int):
+    pk_value = str("F" + number)
+    number += 1
+    return pk_value, number
+
+def generate_pk_dogecoinWallets(number:int):
+    pk_value = str("D" + number)
+    number += 1
+    return pk_value, number
+
+def generate_pk_dogecoinTransactions(number:int):
+    pk_value = str("T" + number)
+    number += 1
+    return pk_value, number
+
+def generate_pk_exchangeOrders(number:int):
+    pk_value = str("E" + number)
+    number += 1
+    return pk_value, number
+
+def generate_external_wallet_id(redundant_set: set):
+    while(True):
+        value = ''.join(random.choices("abcdef" + string.digits, k=64))
+        if not (value in redundant_set):
+            redundant_set.add(value)
+            break
+    return value, redundant_set
+
+def generate_transaction_hash(redundant_set: set):
+    while(True):
+        value = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=34))
+        if not (value in redundant_set):
+            redundant_set.add(value)
+            break
+    return value, redundant_set
