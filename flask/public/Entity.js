c2d5c16d8a781d6c0579d04589eb62cc878d14a1
Kenneth Kang <kangken@flip3.engr.oregonstate.edu>
2022-07-22 00:34:47 -0700

    Update entity.js

--- flask/public/Entity.js
+++ flask/public/Entity.js
@@ -1,1 +1,14 @@
-console.log("triggered");
+// Execute the function when the user presses the return button to search for a
+// database record.
+function didPressReturn(event) {
+    if (event && event.keyCode == 13) {
+        console.log("triggered");
+        return false;
+
+        // Here we need to search for the record and dynamically change the
+        // state of the entity table to display the results.
+
+        // If we enter backspace in the search box, we need to dynamically
+        // change the state of the entity table to display the entire data set.
+    }
+}
